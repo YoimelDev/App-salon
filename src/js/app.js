afd088bbe207bfa7d3d3a22cbecb0802ced5a3f6
YoimelDev <yoimeldev@gmail.com>
2021-07-30 14:42:33 -0400

    function mostrarServicios added

---
+++ src/js/app.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+    iniciarApp();
+})
+
+function iniciarApp() {
+    console.log('Iniciando app');
+    mostrarServicios();
+}
+
+async function mostrarServicios() {
+    try {
+        const resultado = await fetch('./servicios.json');
+        const db = await resultado.json();
+
+        const {servicios} = db;
+
+        // Generar el HTML
+        servicios.forEach( servicio => {
+            const { id, nombre, precio } = servicio;
+
+            // DOM Scripting
+            // Generar nombre de servicio
+            const nombreServicio = document.createElement('P');
+            nombreServicio.textContent = nombre;
+            nombreServicio.classList.add('nombre-servicio');
+
+            // Generar precio del servicio
+            const precioServicio = document.createElement('P');
+            precioServicio.textContent = `$ ${precio}`;
+            precioServicio.classList.add('precio-servicio');
+
+            // Generar div contenedor de servicio
+            const servicioDiv = document.createElement('DIV');
+            servicioDiv.classList.add('servicio');
+
+            // Inyectas Precio y nombre al div de servicio
+            servicioDiv.appendChild(nombreServicio);
+            servicioDiv.appendChild(precioServicio);
+
+            // Inyectar en el HTML
+            document.querySelector('#servicios').appendChild(servicioDiv);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
