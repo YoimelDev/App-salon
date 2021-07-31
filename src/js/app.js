5e5df9f7720bf769aa7333160fdcc49ccd155324
YoimelDev <yoimeldev@gmail.com>
2021-07-31 16:34:54 -0400

    layout paginacion added, function botonesPaginador added and change in the function mostrarSeccion

--- src/js/app.js
+++ src/js/app.js
@@ -17,11 +17,26 @@ function iniciarApp() {
     paginaSiguiente();
 
     paginaAnterior();
+
+    // Comprueba la pagina actual para ocultar o mostrar la paginacion
+    botonesPaginador();
 }
 
 function mostrarSeccion() {
+    // Eliminar mostrar-seccion de la seccion anterior
+    const seccionAnterior = document.querySelector('.mostrar-seccion');
+    if (seccionAnterior) {
+        seccionAnterior.classList.remove('mostrar-seccion');
+    }    
+
     const seccionActual = document.querySelector(`#paso-${pagina}`);
     seccionActual.classList.add('mostrar-seccion');
+
+    // Eliminar la clase de actual en el tab anterior
+    const tabAnterior =  document.querySelector('.tabs button.actual');
+    if (tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }   
 
     // Resalta el Tab Actual
     const tab = document.querySelector(`[data-paso="${pagina}"]`);
@@ -35,21 +50,11 @@ function cambiarSeccion() {
         enlace.addEventListener('click', e => {
             e.preventDefault();
             pagina = parseInt(e.target.dataset.paso);
+         
+            // LLamar la funcion de mostrar seccion
+            mostrarSeccion();
 
-            // Eliminar mostrar-seccion de la seccion anterior
-            document.querySelector('.mostrar-seccion').classList.remove('mostrar-seccion');
-
-
-            // Agrega mostrar-seccion donde dimos click
-            const seccion = document.querySelector(`#paso-${pagina}`);
-            seccion.classList.add('mostrar-seccion');
-
-            // Eliminar la clase de actual en el tab anterior
-            document.querySelector('.tabs button.actual').classList.remove('actual');
-
-            // Agregar la clase de actual en el nuevo tab
-            const tab = document.querySelector(`[data-paso="${pagina}"]`);
-            tab.classList.add('actual');
+            botonesPaginador();
         })
     })
 }
@@ -116,17 +121,34 @@ function seleccionarServicio(e) {
 function paginaSiguiente() {
     const paginaSiguiente = document.querySelector('#siguiente');
     paginaSiguiente.addEventListener('click', () => {
-        pagina++; 
-        
-        console.log(pagina);
+        pagina++;
+
+        botonesPaginador();
     })
 }
 
 function paginaAnterior() {
     const paginaAnterior = document.querySelector('#anterior');
     paginaAnterior.addEventListener('click', () => {
-        pagina--; 
-        
-        console.log(pagina);
+        pagina--;
+
+        botonesPaginador();
     })
+}
+
+function botonesPaginador() {
+    const paginaSiguiente = document.querySelector('#siguiente');
+    const paginaAnterior = document.querySelector('#anterior');
+
+    if(pagina === 1) {
+        paginaAnterior.classList.add('ocultar');
+    } else if (pagina === 3) {
+        paginaSiguiente.classList.add('ocultar');
+        paginaAnterior.classList.remove('ocultar');
+    } else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    }
+
+    mostrarSeccion(); // Cambia la seccion que se muestra por la de la pagina actual
 }
