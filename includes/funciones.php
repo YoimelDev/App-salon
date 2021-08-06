26930a3a6cb334bb1217e337771932ee9b46361c
YoimelDev <yoimeldev@gmail.com>
2021-08-05 22:03:52 -0400

    add array servicios

--- includes/funciones.php
+++ includes/funciones.php
@@ -1,6 +1,6 @@
 <?php
 
-function obtenerServicios()
+function obtenerServicios(): array
 {
     try {
         // Importar una conexion
@@ -10,10 +10,24 @@ function obtenerServicios()
         $sql = "SELECT * FROM servicios;";
         $consulta = mysqli_query($db, $sql);
 
+        // Array vacio
+        $servicios = [];
+        $i = 0;
+
         // Obtener los resultados
-        echo "<pre>";
-        var_dump(mysqli_fetch_assoc($consulta));
-        echo "</pre>";
+        while ($row = mysqli_fetch_assoc($consulta)) {
+            $servicios[$i]['id'] = $row['id'];
+            $servicios[$i]['nombre'] = $row['nombre'];
+            $servicios[$i]['precio'] = $row['precio'];
+
+            $i++;
+        }
+
+        // echo '<pre>';
+        // var_dump($servicios);
+        // echo '</pre>';
+
+        return $servicios;
     } catch (\Throwable $th) {
         //throw $th;
         var_dump($th);
