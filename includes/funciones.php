39c2dc30bca3a894e80ca2747b5e19451ad3a93a
YoimelDev <yoimeldev@gmail.com>
2021-08-05 21:51:03 -0400

    add function obtenerServicios

---
+++ includes/funciones.php
@@ -0,0 +1,24 @@
+<?php
+
+function obtenerServicios()
+{
+    try {
+        // Importar una conexion
+        require 'database.php';
+
+        // Escribir el codigo SQL
+        $sql = "SELECT * FROM servicios;";
+        $consulta = mysqli_query($db, $sql);
+
+        // Obtener los resultados
+        echo "<pre>";
+        var_dump(mysqli_fetch_assoc($consulta));
+        echo "</pre>";
+    } catch (\Throwable $th) {
+        //throw $th;
+        var_dump($th);
+    }
+}
+
+
+obtenerServicios();
